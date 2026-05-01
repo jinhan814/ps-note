98acf80f5277286a3e802a3f839b6bfa4dd89a32
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-05-01 15:47:47 +0900

    .

--- practice1/mascots/main.cpp
+++ practice1/mascots/main.cpp
@@ -22,41 +22,29 @@ constexpr int pow(int x, int n) {
 	return ret;
 }
 
-int bino(int n, int k) {
-	if (k > n - k) k = n - k;
-	int a = 1, b = 1;
-	for (int i = 0; i < k; i++) {
-		a = mul(a, n - i);
-		b = mul(b, i + 1);
-	}
-	return mul(a, pow(b, mod - 2));
-}
-
-int calc(int n, int m, int a, int b) {
-	vector fac(max(n, m) + 1, 1);
-	for (int i = 1; i <= max(n, m); i++) {
+auto sol = [](int n, int m, int k, auto v) {
+	vector fac(n * m + 1, 1);
+	for (int i = 1; i <= n * m; i++) {
 		fac[i] = mul(fac[i - 1], i);
 	}
-	vector dp(n - a + 1, vector(m - b + 1, -1));
+	auto bino = [&](int n, int k) {
+		int v1 = fac[n];
+		int v2 = mul(fac[k], fac[n - k]);
+		return mul(v1, pow(v2, mod - 2));
+	};
+	vector dp(n + 1, vector(m + 1, -1));
 	auto rec = [&](const auto& self, int i, int j) -> int {
 		if (i == n && j == m) return 1;
-		int& ret = dp[i - a][j - b];
+		int& ret = dp[i][j];
 		if (ret != -1) return ret;
 		ret = 0;
 		if (i < n) ret = add(ret, mul(self(self, i + 1, j), fac[j]));
 		if (j < m) ret = add(ret, mul(self(self, i, j + 1), fac[i]));
 		return ret;
 	};
-	return rec(rec, a, b);
-}
-
-int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	int n, m, k; cin >> n >> m >> k;
 	int mn_x = 1 << 30, mx_x = -(1 << 30);
 	int mn_y = 1 << 30, mx_y = -(1 << 30);
-	for (int i = 0; i < k; i++) {
-		int x, y; cin >> x >> y;
+	for (auto [x, y] : v) {
 		mn_x = min(mn_x, x);
 		mx_x = max(mx_x, x);
 		mn_y = min(mn_y, y);
@@ -64,9 +52,17 @@ int main() {
 	}
 	int a = mx_x - mn_x + 1;
 	int b = mx_y - mn_y + 1;
-	int res = calc(n, m, a, b);
-	res = mul(res, bino(n - a, mn_x - 1));
-	res = mul(res, bino(m - b, mn_y - 1));
-	for (int i = 1; i <= a * b - k; i++) res = mul(res, i);
-	cout << res << '\n';
+	int ret = fac[a * b - k];
+	ret = mul(ret, rec(rec, a, b));
+	ret = mul(ret, bino(n - a, mn_x - 1));
+	ret = mul(ret, bino(m - b, mn_y - 1));
+	return ret;
+};
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int n, m, k; cin >> n >> m >> k;
+	vector v(k, array{ 0, 0 });
+	for (int i = 0; i < k; i++) cin >> v[i][0] >> v[i][1];
+	cout << sol(n, m, k, v) << '\n';
 }
