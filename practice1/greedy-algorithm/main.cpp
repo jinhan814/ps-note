b8dd889dda2c40205bdc605ca98dc6fce2072a06
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-05-03 01:33:23 +0900

    .

--- practice1/greedy-algorithm/main.cpp
+++ practice1/greedy-algorithm/main.cpp
@@ -11,35 +11,30 @@ auto calc = [](int n, int m, auto v) {
 			cnt[i][val + 500]++;
 		}
 	}
-	vector dp(1, 0);
-	for (int iter = 1; iter < n; iter++) {
-		vector ndp(1'000 * iter + 1, *max_element(dp.begin(), dp.end()));
-		vector cand(0, 0);
+	vector dp(1'000 * n + 1, -1);
+	dp[500 * n] = 0;
+	for (int iter = 0; iter < n; iter++) {
+		auto ndp = dp;
 		for (int x = -500; x <= 500; x++) {
-			if (cnt[iter][-x + 500] == 0) continue;
-			cand.push_back(x);
-		}
-		for (int i = -500 * (iter - 1); i <= 500 * (iter - 1); i++) {
-			for (int x : cand) {
-				int p = i + x + 500 * iter;
-				ndp[p] = max(ndp[p], dp[i + 500 * (iter - 1)] + cnt[iter][-x + 500]);
+			int val = cnt[iter][x + 500];
+			if (val == 0) continue;
+			for (int i = 0; i <= 1'000 * n; i++) {
+				if (dp[i] == -1) continue;
+				int j = i + x;
+				if (j < 0 || j > 1'000 * n) continue;
+				ndp[j] = max(ndp[j], dp[i] + val);
 			}
 		}
 		dp.swap(ndp);
 	}
 	int ret = 0, mn = 1 << 30;
-	for (int i = 0; i < n; i++) {
-		int mx = -1;
-		for (int x = -500; x <= 500; x++) mx = max(mx, cnt[i][x + 500]);
+	for (int iter = 0; iter < n; iter++) {
+		int mx = *max_element(cnt[iter].begin(), cnt[iter].end());
 		ret += mx;
 		mn = min(mn, mx);
 	}
 	ret -= mn;
-	for (int i = -500 * (n - 1); i <= 500 * (n - 1); i++) {
-		int val = dp[i + 500 * (n - 1)];
-		if (abs(i) <= 500) val += cnt[0][i + 500];
-		ret = max(ret, val);
-	}
+	ret = max(ret, dp[500 * n]);
 	return ret;
 };
 
