0544a96aeb6b8104b03f29522a216380a7f31b41
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-05-03 15:25:44 +0900

    .

--- practice1/fox-and-card-game/main.cpp
+++ practice1/fox-and-card-game/main.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 auto sol = [](int n, auto v) {
 	int v1 = 0, v2 = 0;
-	vector cand(0, 0);
+	vector c(0, 0);
 	for (int i = 0; i < n; i++) {
 		int sz = v[i].size();
 		for (int j = 0; j < sz / 2; j++) {
 			v1 += v[i][j];
 			v2 += v[i][sz - 1 - j];
 		}
-		if (sz % 2 == 1) cand.push_back(v[i][sz / 2]);
+		if (sz % 2 == 1) c.push_back(v[i][sz / 2]);
 	}
-	sort(cand.begin(), cand.end(), greater{});
-	for (int i = 0; i < cand.size(); i++) {
-		if (i % 2 == 0) v1 += cand[i];
-		else v2 += cand[i];
+	sort(c.begin(), c.end(), greater{});
+	for (int i = 0; i < c.size(); i++) {
+		if (i % 2 == 0) v1 += c[i];
+		else v2 += c[i];
 	}
 	return pair(v1, v2);
 };
