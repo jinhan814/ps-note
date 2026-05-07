163593f822273c7cfda3ecb24fac0f78c0f2cb3f
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-05-07 21:30:16 +0900

    .

--- atcoder-ndpc/d/main.cpp
+++ atcoder-ndpc/d/main.cpp
@@ -1,26 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-auto sol = [](string s) {
-	vector dp(s.size(), array{ -1, -1 });
-	auto rec = [&](const auto& self, int i, int f) -> int {
-		if (i < 0) return f;
-		int& ret = dp[i][f];
-		if (ret != -1) return ret;
-		int x = s[i] - 48 + f;
-		int v1 = x + self(self, i - 1, 0);
-		int v2 = 10 - x + self(self, i - 1, 1);
-		ret = min(v1, v2);
-		return ret;
+using i64 = long long;
+
+auto sol = [](i64 n) {
+	map dp{ pair(i64(0), 0), pair(i64(1), 1) };
+	auto rec = [&](const auto& self, i64 n) -> i64 {
+		if (dp.count(n)) return dp[n];
+		i64 v1 = self(self, n / 10) + n % 10;
+		i64 v2 = self(self, n / 10 + 1) + 10 - n % 10;
+		return dp[n] = min(v1, v2);
 	};
-	return rec(rec, s.size() - 1, 0);
+	return rec(rec, n);
 };
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	int tc; cin >> tc;
 	while (tc--) {
-		string s; cin >> s;
-		cout << sol(s) << '\n';
+		i64 n; cin >> n;
+		cout << sol(n) << '\n';
 	}
 }
