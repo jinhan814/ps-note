48a69f4295ed965cc5d8a4a8c89dbf70a7454857
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-05-07 21:01:13 +0900

    .

--- atcoder-ndpc/a/main.cpp
+++ atcoder-ndpc/a/main.cpp
@@ -6,11 +6,11 @@ using i64 = long long;
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	int n; cin >> n;
-	vector dp(41, i64(0));
+	vector dp(max(n, 3), i64(0));
 	dp[0] = 1;
 	dp[1] = 1;
 	dp[2] = 2;
-	for (int i = 3; i <= 40; i++) {
+	for (int i = 3; i <= n; i++) {
 		dp[i] =	2 * dp[i - 1] + dp[i - 3];
 	}
 	cout << dp[n] << '\n';
