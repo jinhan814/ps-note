3aa2c4234afe263382b0ed81ad6693a3050e001b
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-05-25 02:45:08 +0900

    .

--- atcoder/abc457/a/main.cpp
+++ atcoder/abc457/a/main.cpp
@@ -4,12 +4,8 @@ using namespace std;
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	int n; cin >> n;
-	vector v(n, vector(0, 0));
-	for (int i = 0; i < n; i++) {
-		int m; cin >> m;
-		v[i].resize(m);
-		for (int& x : v[i]) cin >> x;
-	}
-	int a, b; cin >> a >> b;
-	cout << v[a - 1][b - 1] << '\n';
+	vector v(n, 0);
+	for (int i = 0; i < n; i++) cin >> v[i];
+	int k; cin >> k;
+	cout << v[k - 1] << '\n';
 }
