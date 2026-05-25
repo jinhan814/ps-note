dbc38a7aa17670f14ef2dbafb35bde3e715f8af9
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-05-25 15:36:31 +0900

    .

---
+++ atcoder/abc450/a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int n; cin >> n;
+	for (int i = n; i >= 1; i--) {
+		if (i != n) cout << ',';
+		cout << i;
+	}
+	cout << '\n';
+}
