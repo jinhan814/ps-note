a4ab7a40d088dcc7072e3be23eb066006323273f
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-05-26 16:52:45 +0900

    .

--- library-checker/data-structure/area_of_union_of_rectangles/main.cpp
+++ library-checker/data-structure/area_of_union_of_rectangles/main.cpp
@@ -15,57 +15,47 @@ struct info {
 };
 
 struct segtree {
-	segtree(int n) : sz(n), tree(1, info(0, n)), link(1, pair(-1, -1)), lazy(1) {}
+	segtree(int n) : sz(1 << (__lg(n - 1 | 1) + 1)), tree(sz << 1), lazy(sz << 1) {}
+	void set(int i, info x) {
+		tree[i |= sz] = x;
+		while (i >>= 1) tree[i] = tree[i << 1] + tree[i << 1 | 1];
+	}
 	void update(int l, int r, int x) {
 		auto rec = [&](const auto& self, int node, int node_l, int node_r) -> void {
-			push(node, node_l, node_r);
+			push(node);
 			if (node_r < l || r < node_l) return;
 			if (l <= node_l && node_r <= r) {
 				lazy[node] = x;
-				push(node, node_l, node_r);
+				push(node);
 				return;
 			}
 			int mid = (node_l + node_r) >> 1;
-			self(self, link[node].first, node_l, mid);
-			self(self, link[node].second, mid + 1, node_r);
-			tree[node] = tree[link[node].first] + tree[link[node].second];
+			self(self, node << 1, node_l, mid);
+			self(self, node << 1 | 1, mid + 1, node_r);
+			tree[node] = tree[node << 1] + tree[node << 1 | 1];
 		};
-		rec(rec, 0, 1, sz);
+		rec(rec, 1, 0, sz - 1);
 	}
 	info query(int l, int r) {
 		auto rec = [&](const auto& self, int node, int node_l, int node_r) -> info {
-			push(node, node_l, node_r);
+			push(node);
 			if (node_r < l || r < node_l) return info(1 << 30, 0);
 			if (l <= node_l && node_r <= r) return tree[node];
 			int mid = (node_l + node_r) >> 1;
-			info r1 = self(self, link[node].first, node_l, mid);
-			info r2 = self(self, link[node].second, mid + 1, node_r);
+			info r1 = self(self, node << 1, node_l, mid);
+			info r2 = self(self, node << 1 | 1, mid + 1, node_r);
 			return r1 + r2;
 		};
-		return rec(rec, 0, 1, sz);
+		return rec(rec, 1, 0, sz - 1);
 	}
 private:
 	int sz;
 	vector<info> tree;
-	vector<pair<int, int>> link;
 	vector<int> lazy;
-	void push(int i, int l, int r) {
-		if (l != r) {
-			int mid = (l + r) >> 1;
-			if (link[i].first == -1) {
-				tree.push_back(info(0, mid - l + 1));
-				link.push_back(pair(-1, -1));
-				lazy.push_back(0);
-				link[i].first = tree.size() - 1;
-			}
-			if (link[i].second == -1) {
-				tree.push_back(info(0, r - mid));
-				link.push_back(pair(-1, -1));
-				lazy.push_back(0);
-				link[i].second = tree.size() - 1;
-			}
-			lazy[link[i].first] += lazy[i];
-			lazy[link[i].second] += lazy[i];
+	void push(int i) {
+		if (i < sz) {
+			lazy[i << 1] += lazy[i];
+			lazy[i << 1 | 1] += lazy[i];
 		}
 		tree[i].mn += lazy[i];
 		lazy[i] = 0;
@@ -74,22 +64,32 @@ private:
 
 auto sol = [](int n, auto v) {
 	vector buc(0, array{ 0, 0, 0, 0 });
+	vector c(0, 0);
 	for (auto [x1, y1, x2, y2] : v) {
-		buc.push_back(array{ x1, 1, y1 + 1, y2 });
-		buc.push_back(array{ x2, -1, y1 + 1, y2 });
+		buc.push_back(array{ x1, 1, y1, y2 });
+		buc.push_back(array{ x2, -1, y1, y2 });
+		c.push_back(y1);
+		c.push_back(y2);
 	}
 	sort(buc.begin(), buc.end());
+	sort(c.begin(), c.end());
+	c.erase(unique(c.begin(), c.end()), c.end());
 	i64 ret = 0;
-	segtree tree(1'000'000'000);
+	segtree tree(c.size() - 1);
+	for (int i = 0; i < c.size() - 1; i++) {
+		tree.set(i, info(0, c[i + 1] - c[i]));
+	}
 	for (int i = 0, j = 0; i < buc.size(); i = j) {
 		while (j < buc.size() && buc[i][0] == buc[j][0]) {
 			auto [x, d, l, r] = buc[j++];
-			tree.update(l, r, d);
+			l = lower_bound(c.begin(), c.end(), l) - c.begin();
+			r = lower_bound(c.begin(), c.end(), r) - c.begin();
+			tree.update(l, r - 1, d);
 		}
 		if (j < buc.size()) {
-			info res = tree.query(1, 1'000'000'000);
+			info res = tree.query(0, c.size() - 2);
 			i64 v1 = buc[j][0] - buc[i][0];
-			i64 v2 = 1'000'000'000;
+			i64 v2 = c.back() - c[0];
 			if (res.mn == 0) v2 -= res.cnt;
 			ret += v1 * v2;
 		}
