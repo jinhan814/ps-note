7c1ee6a30ab4ed15443acfa12cef00458773a459
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-05-27 22:25:12 +0900

    .

--- atcoder/abc448/e/main.cpp
+++ atcoder/abc448/e/main.cpp
@@ -7,10 +7,6 @@ auto modadd = [](int a, int b, int mod) {
 	return a + b < mod ? a + b : a + b - mod;
 };
 
-auto modsub = [](int a, int b, int mod) {
-	return a < b ? a - b + mod : a - b;
-};
-
 auto modmul = [](int a, int b, int mod) {
 	return i64(a) * b % mod;
 };
@@ -25,27 +21,15 @@ auto modpow = [](int x, i64 n, int mod) {
 };
 
 auto sol = [](int n, int m, auto a, auto b) {
-	int m1 = 9 * m;
-	int m2 = 10'007;
+	int mod = 10'007 * m;
 	int ret = 0;
-	int acc = 0;
-	i64 s = 0;
-	for (int i = n - 1; i >= 0; i--) {
-		int v1 = a[i] % m1;
-		v1 = modmul(v1, modsub(modpow(10 % m1, b[i], m1), 1, m1), m1);
-		v1 = modmul(v1, modpow(10 % m1, s, m1), m1);
-		v1 /= 9;
-		int v2 = a[i] % m2;
-		v2 = modmul(v2, modsub(modpow(10 % m2, b[i], m2), 1, m2), m2);
-		v2 = modmul(v2, modpow(10 % m2, s, m2), m2);
-		v2 = modmul(v2, modpow(9, m2 - 2, m2), m2);
-		v2 = modsub(v2, v1, m2);
-		v2 = modmul(v2, modpow(m, m2 - 2, m2), m2);
-		ret = modadd(ret, v2, m2);
-		acc += v1;
-		s += b[i];
+	for (int i = 0; i < n; i++) {
+		int v1 = modpow(10, b[i], 9 * mod);
+		int v2 = modmul(a[i], modadd(v1, 9 * mod - 1, 9 * mod), 9 * mod);
+		ret = modmul(ret, v1 % mod, mod);
+		ret = modadd(ret, v2 / 9, mod);
 	}
-	ret = modadd(ret, acc / m % m2, m2);
+	ret = ret / m % 10'007;
 	return ret;
 };
 
