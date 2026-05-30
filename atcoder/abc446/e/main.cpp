05abeee5b14b9ba2337581bd71f426d1f79bec44
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-05-31 01:20:47 +0900

    .

--- atcoder/abc446/e/main.cpp
+++ atcoder/abc446/e/main.cpp
@@ -2,57 +2,35 @@
 using namespace std;
 
 auto sol = [](int n, int a, int b) {
-	vector nxt(n, vector(n, pair(0, 0)));
+	vector adj(n, vector(n, vector(0, 0)));
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			nxt[i][j].first = j;
-			nxt[i][j].second = (b * i + a * j) % n;
+			int k = (a * j + b * i) % n;
+			adj[j][k].push_back(i);
 		}
 	}
-	vector c(n, vector(n, 0));
-	vector d(n, vector(n, -1));
+	vector c(n, vector(n, false));
+	vector q(0, pair(0, 0));
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (c[i][j] != 0) continue;
-			int x = i, y = j;
-			while (c[x][y] == 0) {
-				c[x][y] = 1;
-				tie(x, y) = nxt[x][y];
-			}
-			if (c[x][y] == 1) {
-				int px = x, py = y;
-				int val = 0;
-				while (1) {
-					if (x == 0 || y == 0) val = 1;
-					tie(x, y) = nxt[x][y];
-					if (x == px && y == py) break;
-				}
-				while (1) {
-					d[x][y] = val;
-					tie(x, y) = nxt[x][y];
-					if (x == px && y == py) break;
-				}
-			}
-			x = i, y = j;
-			while (c[x][y] == 1) {
-				c[x][y] = 2;
-				tie(x, y) = nxt[x][y];
-			}
+		c[0][i] = true;
+		q.push_back(pair(0, i));
+		if (i > 0) {
+			c[i][0] = true;
+			q.push_back(pair(i, 0));
 		}
 	}
-	auto rec = [&](const auto& self, int i, int j) -> int {
-		int& ret = d[i][j];
-		if (ret != -1) return ret;
-		ret = 0;
-		if (i == 0 || j == 0) ret = 1;
-		auto [x, y] = nxt[i][j];
-		if (self(self, x, y)) ret = 1;
-		return ret;
-	};
+	while (q.size()) {
+		auto [j, k] = q.back(); q.pop_back();
+		for (int i : adj[j][k]) {
+			if (c[i][j]) continue;
+			c[i][j] = true;
+			q.push_back(pair(i, j));
+		}
+	}
 	int ret = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (rec(rec, i, j)) continue;
+			if (c[i][j]) continue;
 			ret++;
 		}
 	}
