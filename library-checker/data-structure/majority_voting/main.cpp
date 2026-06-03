fd14b9f5d5785e3adf3dd169a91d9e8f6d5cccbc
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-06-03 21:56:59 +0900

    .

--- library-checker/data-structure/majority_voting/main.cpp
+++ library-checker/data-structure/majority_voting/main.cpp
@@ -1,33 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct fenwick {
-	fenwick(int n) : sz(n), tree(n + 1) {}
-	void update(int i, int x) {
-		for (; i <= sz; i += i & -i) tree[i] += x;
-	}
-	int query(int i) const {
-		int ret = 0;
-		for (; i; i -= i & -i) ret += tree[i];
-		return ret;
-	}
-	int query(int l, int r) const {
-		return query(r) - query(l - 1);
-	}
-private:
-	int sz;
-	vector<int> tree;
-};
-
 struct node {
 	int cnt, val;
 	node() : cnt(0), val(-1) {}
 	node(int x) : cnt(1), val(x) {}
 	node(int cnt, int val) : cnt(cnt), val(val) {}
 	node operator+ (const node& x) const {
-		if (val == -1) return x;
-		if (x.val == -1) return *this;
 		if (val == x.val) return node(cnt + x.cnt, val);
+		if (cnt == x.cnt) return node();
 		if (cnt > x.cnt) return node(cnt - x.cnt, val);
 		return node(x.cnt - cnt, x.val);
 	}
@@ -52,6 +33,24 @@ private:
 	vector<node> tree;
 };
 
+struct fenwick {
+	fenwick(int n) : sz(n), tree(n + 1) {}
+	void update(int i, int x) {
+		for (; i <= sz; i += i & -i) tree[i] += x;
+	}
+	int query(int i) const {
+		int ret = 0;
+		for (; i; i -= i & -i) ret += tree[i];
+		return ret;
+	}
+	int query(int l, int r) const {
+		return query(r) - query(l - 1);
+	}
+private:
+	int sz;
+	vector<int> tree;
+};
+
 auto sol = [](int n, int q, auto v, auto qs) {
 	vector c(0, pair(0, 0));
 	for (int i = 0; i < n; i++) {
@@ -63,26 +62,24 @@ auto sol = [](int n, int q, auto v, auto qs) {
 	}
 	sort(c.begin(), c.end());
 	c.erase(unique(c.begin(), c.end()), c.end());
-	fenwick t1(c.size());
-	segtree t2(n);
+	segtree t1(n);
+	fenwick t2(c.size());
 	for (int i = 0; i < n; i++) {
-		int p = lower_bound(c.begin(), c.end(), pair(v[i], i)) - c.begin() + 1;
-		t1.update(p, 1);
-		t2.update(i, node(v[i]));
+		t1.update(i, node(v[i]));
+		t2.update(lower_bound(c.begin(), c.end(), pair(v[i], i)) - c.begin() + 1, 1);
 	}
 	auto update = [&](int i, int x) {
-		int p1 = lower_bound(c.begin(), c.end(), pair(v[i], i)) - c.begin() + 1;
-		t1.update(p1, -1);
+		t2.update(lower_bound(c.begin(), c.end(), pair(v[i], i)) - c.begin() + 1, -1);
 		v[i] = x;
-		int p2 = lower_bound(c.begin(), c.end(), pair(v[i], i)) - c.begin() + 1;
-		t1.update(p2, 1);
-		t2.update(i, node(v[i]));
+		t1.update(i, node(v[i]));
+		t2.update(lower_bound(c.begin(), c.end(), pair(v[i], i)) - c.begin() + 1, 1);
 	};
 	auto query = [&](int l, int r) {
-		int val = t2.query(l, r).val;
+		int val = t1.query(l, r).val;
+		if (val == -1) return -1;
 		int p1 = lower_bound(c.begin(), c.end(), pair(val, l)) - c.begin() + 1;
 		int p2 = upper_bound(c.begin(), c.end(), pair(val, r)) - c.begin();
-		int cnt = t1.query(p1, p2);
+		int cnt = t2.query(p1, p2);
 		if (cnt <= (r - l + 1) / 2) return -1;
 		return val;
 	};
