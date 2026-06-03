b1eacaf78d131d6eb44c21297135c4648a152722
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-06-03 16:22:46 +0900

    .

--- library-checker/data-structure/README.md
+++ library-checker/data-structure/README.md
@@ -25,7 +25,7 @@
 | 12 | Point Add Range Sum | https://judge.yosupo.jp/problem/point_add_range_sum | [source code](./point_add_range_sum/main.cpp) |
 | 13 | Point Set Range Composite | https://judge.yosupo.jp/problem/point_set_range_composite | [source code](./point_set_range_composite/main.cpp) |
 | 14 | Point Set Range Composite (Large Array) | https://judge.yosupo.jp/problem/point_set_range_composite_large_array | [source code](./point_set_range_composite_large_array/main.cpp) |
-| 15 | Range Add Range Min | https://judge.yosupo.jp/problem/range_add_range_min | |
+| 15 | Range Add Range Min | https://judge.yosupo.jp/problem/range_add_range_min | [source code](./range_add_range_min/main.cpp) |
 | 16 | Range Affine Point Get | https://judge.yosupo.jp/problem/range_affine_point_get | [source code](./range_affine_point_get/main.cpp) |
 | 17 | Range Affine Range Sum | https://judge.yosupo.jp/problem/range_affine_range_sum | [source code](./range_affine_range_sum/main.cpp) |
 | 18 | Range Affine Range Sum (Large Array) | https://judge.yosupo.jp/problem/range_affine_range_sum_large_array | [source code](./range_affine_range_sum_large_array/main.cpp) |
---
+++ library-checker/data-structure/range_add_range_min/main.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using i64 = long long;
+
+struct segtree {
+	segtree(int n) : sz(1 << (__lg(n - 1 | 1) + 1)), tree(sz << 1), lazy(sz << 1) {}
+	void update(int l, int r, int x) {
+		auto rec = [&](const auto& self, int node, int node_l, int node_r) -> void {
+			push(node);
+			if (node_r < l || r < node_l) return;
+			if (l <= node_l && node_r <= r) {
+				lazy[node] = x;
+				push(node);
+				return;
+			}
+			int mid = (node_l + node_r) >> 1;
+			self(self, node << 1, node_l, mid);
+			self(self, node << 1 | 1, mid + 1, node_r);
+			tree[node] = min(tree[node << 1], tree[node << 1 | 1]);
+		};
+		rec(rec, 1, 0, sz - 1);
+	}
+	i64 query(int l, int r) {
+		auto rec = [&](const auto& self, int node, int node_l, int node_r) -> i64 {
+			push(node);
+			if (node_r < l || r < node_l) return i64(1) << 60;
+			if (l <= node_l && node_r <= r) return tree[node];
+			int mid = (node_l + node_r) >> 1;
+			i64 r1 = self(self, node << 1, node_l, mid);
+			i64 r2 = self(self, node << 1 | 1, mid + 1, node_r);
+			return min(r1, r2);
+		};
+		return rec(rec, 1, 0, sz - 1);
+	}
+private:
+	int sz;
+	vector<i64> tree;
+	vector<i64> lazy;
+	void push(int i) {
+		if (i < sz) {
+			lazy[i << 1] += lazy[i];
+			lazy[i << 1 | 1] += lazy[i];
+		}
+		tree[i] += lazy[i];
+		lazy[i] = 0;
+	}
+};
+
+auto sol = [](int n, int q, auto v, auto qs) {
+	segtree tree(n);
+	for (int i = 0; i < n; i++) tree.update(i, i, v[i]);
+	vector ret(0, i64(0));
+	for (auto [op, a, b, c] : qs) {
+		if (op == 0) tree.update(a, b - 1, c);
+		else ret.push_back(tree.query(a, b - 1));
+	}
+	return ret;
+};
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int n, q; cin >> n >> q;
+	vector v(n, 0);
+	vector qs(q, array{ 0, 0, 0, 0 });
+	for (int i = 0; i < n; i++) cin >> v[i];
+	for (auto& [op, a, b, c] : qs) {
+		cin >> op >> a >> b;
+		if (op == 0) cin >> c;
+	}
+	auto res = sol(n, q, v, qs);
+	for (i64 x : res) cout << x << '\n';
+}
