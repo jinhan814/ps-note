8229f00dd8a998300c6d0953ea7cdb264866a464
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-06-06 16:08:22 +0900

    .

---
+++ general/practice1/nim-without-zero/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+auto sol = [](int n, auto v) {
+	int acc = 0, cnt = 0;
+	for (int i = 0; i < n; i++) {
+		acc ^= v[i];
+		if (v[i] % 2 == 1) cnt++;
+	}
+	if (acc == 1) return false;
+	if (acc == 0 && cnt == 0) return false;
+	return true;
+};
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int n; cin >> n;
+	vector v(n, 0);
+	for (int i = 0; i < n; i++) cin >> v[i];
+	cout << (sol(n, v) ? "Alice" : "Bob") << '\n';
+}
