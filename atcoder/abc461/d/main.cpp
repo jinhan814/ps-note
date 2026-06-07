04fb1e15e8e177de66b35b5fa7d43e1d9f4ff3d6
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-06-07 21:30:50 +0900

    .

--- atcoder/abc461/d/main.cpp
+++ atcoder/abc461/d/main.cpp
@@ -4,11 +4,15 @@ using namespace std;
 using i64 = long long;
 
 auto calc = [](auto v, int k) {
-	if (k < 0) return 0;
-	int ret = 0, acc = 0;
-	for (int i = 0, j = 0; i < v.size(); acc -= v[i++]) {
-		while (j < v.size() && acc + v[j] <= k) acc += v[j++];
-		ret += j - i;
+	vector p(v.size() + 1, 0);
+	for (int i = 0; i < v.size(); i++) p[i + 1] = p[i] + v[i];
+	int ret = 0;
+	for (int i = 0, p1 = 0, p2 = 0; i < v.size(); i++) {
+		if (p1 < i) p1 = i;
+		if (p2 < i) p2 = i;
+		while (p1 < v.size() && p[p1 + 1] - p[i] <= k) p1++;
+		while (p2 < v.size() && p[p2 + 1] - p[i] <= k - 1) p2++;
+		ret += p1 - p2;
 	}
 	return ret;
 };
@@ -22,7 +26,7 @@ auto sol = [](int n, int m, int k, auto v) {
 				if (v[i][r] == '0') continue;
 				c[i]++;
 			}
-			ret += calc(c, k) - calc(c, k - 1);
+			ret += calc(c, k);
 		}
 	}
 	return ret;
