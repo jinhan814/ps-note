d99d05bec468d49e2486a2d543a6d5b3c9e53287
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-07-16 11:49:13 +0900

    .

--- atcoder/abc440/e/main.cpp
+++ atcoder/abc440/e/main.cpp
@@ -5,30 +5,17 @@ using i64 = long long;
 
 auto sol = [](int n, int m, int k, auto v) {
 	sort(v.begin(), v.end(), greater{});
-	set s{ vector(0, 0) }; s.clear();
-	set pq{ pair(i64(0), vector(0, 0)) }; pq.clear();
-	auto push = [&](auto c) {
-		if (s.count(c)) return;
-		i64 acc = 0;
-		for (int i = 0; i < n; i++) acc += i64(c[i]) * v[i];
-		s.insert(c);
-		pq.insert(pair(acc, c));
-	};
-	vector c(n, 0);
-	c[0] = m;
-	push(c);
 	vector ret(0, i64(0));
+	multiset pq{ tuple(i64(m) * v[0], 0, 0, m) };
 	while (ret.size() < k) {
-		auto [val, c] = *prev(pq.end());
+		auto [val, i, a, b] = *prev(pq.end());
 		pq.erase(prev(pq.end()));
 		ret.push_back(val);
-		for (int i = 1; i < n; i++) {
-			if (c[i - 1] == 0) continue;
-			c[i - 1]--;
-			c[i]++;
-			push(c);
-			c[i - 1]++;
-			c[i]--;
+		if (a > 0) {
+			pq.insert(tuple(val - v[i - 1] + v[i], i, a - 1, b + 1));
+		}
+		if (i + 1 < n) {
+			pq.insert(tuple(val - v[i] + v[i + 1], i + 1, b - 1, 1));
 		}
 	}
 	return ret;
