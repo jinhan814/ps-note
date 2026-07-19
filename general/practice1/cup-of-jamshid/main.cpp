01de5cf36b8eb170143fc05eb6742276b5dfc67a
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-07-19 15:12:13 +0900

    .

--- general/practice1/cup-of-jamshid/main.cpp
+++ general/practice1/cup-of-jamshid/main.cpp
@@ -2,14 +2,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int lim = 1'000'000'000;
+constexpr int pos = -500'000'001;
 
 vector<int> find_cup() {
-	int val = ask_shahrasb(-lim, -lim);
+	auto ask = [](int x, int y) { return ask_shahrasb(x + pos, y + pos); };
+	int val = ask(0, 0);
 	int acc = 0;
-	for (int bit = 0; bit <= 30; bit++) {
-		int res = ask_shahrasb((1 << bit) - lim, -lim);
+	for (int bit = 0; bit < 30; bit++) {
+		int res = ask(1 << bit, 0);
 		if (res == (val ^ (1 << bit))) acc ^= 1 << bit;
 	}
-	return { acc - lim, (val ^ acc) - lim };
+	return { pos + acc, pos + (val ^ acc) };
 }
