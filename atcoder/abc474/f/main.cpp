4078685a713c64cb5a2403a76f3743d0408871b5
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2026-09-06 23:03:32 +0900

    .

--- atcoder/abc474/f/main.cpp
+++ atcoder/abc474/f/main.cpp
@@ -5,8 +5,10 @@ using i64 = long long;
 
 auto sol = [](int n, auto v) {
 	vector c(n + 1, array{ i64(0), i64(0) });
-	i64 sa = 0, sb = 0;
-	i64 lo = 0, hi = 2'000'000'000;
+	i64 lo = *max_element(v.begin() + 1, v.end());
+	i64 hi = 2 * lo;
+	i64 sa = 0;
+	i64 sb = 0;
 	for (int i = n; i >= 1; i--) {
 		auto& [a, b] = c[i];
 		a = 1;
